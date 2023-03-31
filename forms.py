984ece268ad8091db7acec25eb6387934844f653
Anntymchenko <124369819+Anntymchenko@users.noreply.github.com>
2023-03-31 11:57:51 +0300

    Create forms.py

---
+++ forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Listing
+
+
+class ListingForm(forms.ModelForm):
+    class Meta:
+        model = Listing
+        fields = ['title', 'description', 'price', 'image', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
