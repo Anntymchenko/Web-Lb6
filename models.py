67b240e1df8c483942d05125686efb8fb696d879
Anntymchenko <124369819+Anntymchenko@users.noreply.github.com>
2023-03-31 12:08:00 +0300

    Update models.py

--- models.py
+++ models.py
@@ -33,3 +33,54 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment by {self.user.username} on {self.ad.title}'
+    
+    from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    
+    from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    
+    from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+class Subcategory(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Ad(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='ads_photos/', null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    condition_choices = (
+        ('new', 'New'),
+        ('used', 'Used'),
+        ('refurbished', 'Refurbished'),
+    )
+    condition = models.CharField(max_length=20, choices=condition_choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    contact_info = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+
+    from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    is_customer = models.BooleanField(default=False)
+    is_seller = models.BooleanField(default=False)
+
